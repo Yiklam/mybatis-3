b945c308a6e187a91b54248ca7854064ba4fed88
陈其苗 <chenqimiao1994@126.com>
2020-03-18 21:38:39 +0800

    Polishing

--- src/test/java/com/github/chenqimiao/MybatisTests.java
+++ src/test/java/com/github/chenqimiao/MybatisTests.java
@@ -18,31 +18,21 @@ import org.junit.Test;
 public class MybatisTests {
 
   @Test
-  public void test() {
-    String resource="mybatis-config.xml";
-    InputStream inputStream=null;
-    try {
-      inputStream = Resources.getResourceAsStream(resource);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    SqlSessionFactory sqlSessionFactory=null;
-    sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
-    SqlSession sqlSession=null;
-    try {
-      sqlSession=sqlSessionFactory.openSession();
-      RoleMapper roleMapper=sqlSession.getMapper(RoleMapper.class);
-      Role role=roleMapper.getRole(1L);
-      System.out.println(role.getId()+":"+role.getRoleName()+":"+role.getNote());
-      sqlSession.commit();
-
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      sqlSession.rollback();
-      e.printStackTrace();
-    }finally {
-      sqlSession.close();
+  public void test() throws IOException {
+    String resource = "mybatis-config.xml";
+    InputStream inputStream = Resources.getResourceAsStream(resource);
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      try {
+        RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+        Role role = roleMapper.getRole(1L);
+        System.out.println(role);
+        sqlSession.commit();
+      }catch (Exception e) {
+        // TODO Auto-generated catch block
+        sqlSession.rollback();
+        e.printStackTrace();
+      }
     }
   }
 }
